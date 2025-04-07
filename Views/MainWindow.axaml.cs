3e3e4bac4fa7b75a0ab4612a026322ef3c839bd1
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-04-08 00:18:24 +0100

    Completed the program
    This is a simple basic test program where I look at Avaloonia UI framework.

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -14,6 +14,19 @@ public partial class MainWindow : Window
 
     private void Button_Click(object? sender, RoutedEventArgs e)
     {
-        Debug.WriteLine("Button Clicked");
+        Debug.WriteLine($"Button Clicked! Celsius={Celsius.Text}");
+        Debug.WriteLine($"Button Clicked! Celsius={Fahrenheit.Text}");
+
+        if (double.TryParse(Celsius.Text, out double C))
+        {
+            var F = C * (9d / 5d) + 32;
+            Fahrenheit.Text = F.ToString("0.0");
+        }
+        else
+        {
+            Celsius.Text = "0";
+            Fahrenheit.Text = "0";
+        }
+
     }
 }
