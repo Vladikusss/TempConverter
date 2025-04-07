96a3bc042521514b88364534b2120a0a460e1661
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-04-08 00:00:46 +0100

    Created Clickable Button
    Added debug into clickable button

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Diagnostics;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace TempConverter.Views;
 
@@ -7,5 +10,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_Click(object? sender, RoutedEventArgs e)
+    {
+        Debug.WriteLine("Button Clicked");
     }
 }
